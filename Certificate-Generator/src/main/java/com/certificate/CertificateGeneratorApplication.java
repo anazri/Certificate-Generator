3d68d31444eb91882e711c3c1bf906e2672b9121
mirkomikac <mmikac27@gmail.com>
2017-06-20 11:50:13 +0200

    HTTPS support.

--- Certificate-Generator/src/main/java/com/certificate/CertificateGeneratorApplication.java
+++ Certificate-Generator/src/main/java/com/certificate/CertificateGeneratorApplication.java
@@ -1,7 +1,9 @@
 package com.certificate;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CertificateGeneratorApplication {
@@ -9,4 +11,12 @@ public class CertificateGeneratorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CertificateGeneratorApplication.class, args);
 	}
+	
+    @Bean
+	CommandLineRunner lookup() {
+		return args -> {
+			SSLCertificateValidation.disable();
+		};
+	}
+	
 }
